c90f14806fcdd24aee02813a19a9a5ddf962e10d
Jmunapo <jmunapo@outlook.com>
2019-02-19 14:29:17 +0200

    Initial commit

---
+++ bot.js
@@ -0,0 +1,26 @@
+const Telegraf = require('telegraf');
+const { bot_key } = require('./env.json');
+
+const session = require('telegraf/session');
+const bot = new Telegraf(bot_key);
+
+
+bot.use(session())
+
+
+
+
+
+bot.action(/.+/, (ctx) => {
+  ctx.editMessageReplyMarkup(null);
+  return ctx.answerCbQuery(`Unrecognized command. Say what?`);
+});
+
+bot.catch((err) => {
+  console.log('Ooops', err)
+});
+
+setImmediate(() => {
+  bot.startPolling()
+  console.log('Dev SaruBot')
+});
